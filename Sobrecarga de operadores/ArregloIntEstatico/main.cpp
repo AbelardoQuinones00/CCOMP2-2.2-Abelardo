27b48b3b0fa7f49a049fec8269d673518d47663e
Kanczs <kelvin.caya@ucsp.edu.pe>
2022-11-10 08:33:36 -0500

    Sobrecarga

---
+++ Sobrecarga de operadores/ArregloIntEstatico/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "IntArray.h"
+
+using namespace std;
+
+int main() {
+
+    IntArray a(2);    
+
+    a.setAt(0, 34);
+    a.setAt(1, 35);
+
+    a.print();
+
+    cout << a << endl;    
+ 
+    return 0;
+}
